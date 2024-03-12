533ca55018af914fe70be3ba5232650f834c15ef
Sahil Rajwar <sahilrajwar2004@gmail.com>
2024-03-12 23:40:50 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -2,18 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cpp",
-    version = "1.0",
+    version = "1.1",
     packages = find_packages(),
     author = "Sahil Rajwar",
-    description = "A Python class for compiling & running C++ code",
-    long_description = open('README.md').read(),
+    description = "A Python lib for compiling & running C++ code",
+    long_description = open("README.md","r").read(),
     long_description_content_type = 'text/markdown',
     url = "https://github.com/Sahil-Rajwar-2004/cpp.py",
-    license = "MIT",
-    classifiers=[
+    license = open("LICENSE.txt","r").read(),
+    classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
-
